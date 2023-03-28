2af310cd4e6db0373cf060bc782c05551336ce7c
agonzalesc <agonzalesc@unal.edu.co>
2023-03-27 23:33:57 -0500

    Agregando estilos css y sass a la aplicación para poder ir legando a la demo app que se tenía en los anteriores commits

--- product-and-cart/src/main.js
+++ product-and-cart/src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router'                                                               
+
+import './assets/styles/style.scss'
 
 const app = createApp(App)
 app.use(router)
